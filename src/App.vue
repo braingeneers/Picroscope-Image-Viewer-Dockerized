69fb61c1e00849abc609335c40d2fac6d60cc342
Pierre Baudin <pvbaudin@ucsc.edu>
2022-04-11 17:42:29 -0700

    Revert "well that failed..."
    This reverts commit 91a059998903652a469f4315c88ef9fd3dc6a5a0.

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,98 @@
-<div id="app">
-  <h1>Hello App!</h1>
-  <p>
-    <!-- use the router-link component for navigation. -->
-    <!-- specify the link by passing the `to` prop. -->
-    <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
-    <router-link to="/">Go to Home</router-link>
-    <!-- <router-link to="/about">Go to About</router-link> -->
-  </p>
-  <!-- route outlet -->
-  <!-- component matched by the route will render here -->
-  <router-view></router-view>
-</div>
+<template>
+  <div id="app">
+    <!-- <router-view></router-view> -->
+    <img alt="Braingeneers Imaging" src="./assets/logo.png">
+    <label>uuid</label>
+    <input v-model="uuid" placeholder="Ryan-4-2020-02-04">
+    <label>group identifier</label>
+    <input v-model="groupID" placeholder="C">
+    <div class="mySelectDiv">
+      <v-select v-model="uuid" :options="uuid_options"></v-select>
+      <v-select v-model="groupID" :options="groupID_options"></v-select>
+    </div>
+    <button v-on:click="$refs.images.loader('newest')">Load Latest</button>
+    <button v-on:click="$refs.images.loader('oldest')">Load Beginning</button>
+    <button v-on:click="$refs.images.loader('sync')">Sync</button>
+
+    <!-- <button v-on:click="uuid='boof'">barf</button> -->
+    <Images ref="images" v-bind:groupID="groupID" v-bind:uuid="uuid" v-bind:endpoint="endpoint"/>
+  </div>
+</template>
+
 
 <script>
-import MainView from "./components/mainView.vue"
+import Images from "./components/Images.vue"
 //import Images2 from "./components/Images2.vue"
 export default {
   name: "app",
   components: {
-    MainView,
+    Images,
     //Images2
   },
+  data: function () {
+    return {
+      uuid: "2022-03-23-i-UCB-bio",
+      groupID: "G",
+      endpoint: "https://s3-west.nrp-nautilus.io/braingeneers/imaging",
+      uuid_options: [],
+      groupID_options:['A','B','C','D','E','F','G','H']
+      // options: [
+      //   { text: 'One', value: 'A' },
+      //   { text: 'Two', value: 'B' },
+      //   { text: 'Three', value: 'C' }
+      // ]
+    }
+  },
+  mounted() {
+    fetch(`${this.endpoint}/uuid-manifest.txt`)
+    .then(response => response.text())
+    .then(textString => {
+      const lines = textString.split('\n');
+      for (var line of lines){
+        this.uuid_options.push(line.substring(0, line.length-1))
+        console.log(line.substring(0, line.length-1))
+      }
+      //console.log(textString);
+    });
+    console.log('Component has been created!');
+    // console.log(poo)
+  }
 }
 </script>
 
 <style>
-  /* .mySelectDiv {
-  max-width: 500px !important;
-  } */
+/* .mySelectDiv {
+max-width: 500px !important;
+} */
 
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
+<!-- /* <style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style> */ -->
